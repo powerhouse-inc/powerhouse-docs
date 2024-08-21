3755fbc7d62f61e237e6343d2008a6dec4f27033
liberuum <liberom@proton.me>
2024-08-21 12:13:03 +0100

    remove tutorial

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -67,12 +67,6 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {
-          type: 'docSidebar',
           sidebarId: 'connectSidebar',
           position: 'left',
           label: 'Connect',
