f561c521e7a67e025540c0ee827d567ba56d4bfc
liberuum <liberom@proton.me>
2024-08-30 12:17:57 +0100

    gh pages config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,6 +20,8 @@ const config: Config = {
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
